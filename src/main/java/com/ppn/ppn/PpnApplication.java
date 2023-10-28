cc206eff6f32ae1e671bbc5b58152c3529079ad0
letung999 <letung1112000@gmail.com>
2023-10-28 10:06:29 +0700

    create structure of project

---
+++ src/main/java/com/ppn/ppn/PpnApplication.java
@@ -0,0 +1,13 @@
+package com.ppn.ppn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ppn/ppn/PpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppn.ppn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
