9ab14bae830a7a9454298a58f825b90a434eee50
letung999 <letung1112000@gmail.com>
2023-11-01 09:18:32 +0700

    check if parameter exists do nothing

--- main.tf
+++ main.tf
@@ -2,4 +2,18 @@ resource "aws_ssm_parameter" "ssm" {
   name  = "/ppn/silo-5/url-db"
   type  = "String"
   value = "none"
+
+  tags = {
+    environment = "silo-5"
+  }
+
+  overwrite = true
+
+  lifecycle {
+    ignore_changes = [
+      value,
+      name,
+      tags
+    ]
+  }
 }
