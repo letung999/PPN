3e2b2c6b39a5457a3cc44f1a5bba2d5ced9b4d85
letung999 <letung1112000@gmail.com>
2023-11-06 11:12:09 +0700

    override MethodArgumentNotValidException and handle exception

--- src/main/java/com/ppn/ppn/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ppn/ppn/exception/GlobalExceptionHandler.java
@@ -1,4 +1,38 @@
 package com.ppn.ppn.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode statusCode,
+                                                                  WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(e -> {
+            String fieldName = ((FieldError) e).getField();
+            String message = e.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        ErrorInvalidArgument errorInvalidArgument = ErrorInvalidArgument.builder()
+                .path(webRequest.getDescription(false))
+                .status(HttpStatusCode.valueOf(400).toString())
+                .localDateTime(LocalDateTime.now())
+                .error(errors)
+                .build();
+        return new ResponseEntity<>(errorInvalidArgument, HttpStatus.BAD_REQUEST);
+    }
 }
