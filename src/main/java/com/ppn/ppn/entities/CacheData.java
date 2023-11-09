05a1949e9376b6eab1683f5d52fd109fe7162628
LeTung <66472197+letung999@users.noreply.github.com>
2023-11-09 14:12:01 +0700

    Implement Redis to cache data (#28)
    * implement authentication and fix error security
    
    * refactor name version of dependencies
    
    * active prod environment
    
    * modify relationship entities to get data
    
    * add message status to response
    
    * turn on jpa auditing
    
    * add correlationId
    
    * handle verify email
    
    * override MethodArgumentNotValidException and handle exception
    
    * create role api
    
    * fix error verify email
    
    * remove redudant class
    
    * create a general for response api and modify response at controller
    
    * handle upload file amazon s3
    
    * dÆhandle dÆÆownload file from s3
    
    * implement redis server
    
    ---------
    
    Co-authored-by: letung999 <letung1112000@gmail.com>

---
+++ src/main/java/com/ppn/ppn/entities/CacheData.java
@@ -0,0 +1,25 @@
+package com.ppn.ppn.entities;
+
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@RedisHash("cacheData")
+public class CacheData {
+    @Id
+    private String key;
+
+    @Indexed
+    private String value;
+}
--- src/main/java/com/ppn/ppn/service/constract/IFileUploadToS3Service.java
+++ src/main/java/com/ppn/ppn/service/constract/IFileUploadToS3Service.java
@@ -1,6 +1,7 @@
 package com.ppn.ppn.service.constract;
 
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.util.List;
 
