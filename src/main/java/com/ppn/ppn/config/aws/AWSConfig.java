2ccee0fa8af897452ffbb745779085d4330dc6ad
letung999 <letung1112000@gmail.com>
2023-11-14 10:45:09 +0700

    set up monitoring log by CloudWatch

--- src/main/java/com/ppn/ppn/config/aws/AWSConfig.java
+++ src/main/java/com/ppn/ppn/config/aws/AWSConfig.java
@@ -4,6 +4,8 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.logs.AWSLogs;
+import com.amazonaws.services.logs.AWSLogsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,10 +20,25 @@ public class AWSConfig {
     @Value("${aws.ppn.accessKey}")
     private String accessKey;
 
+    @Value("${aws.ppn.log.stream.name}")
+    private String logStreamName;
+
+    @Value("${aws.ppn.log.group.name}")
+    private String logGroupName;
+
     @Bean
-    public AmazonS3 initS3Client(){
+    public AmazonS3 initS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
         return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+    @Bean
+    public AWSLogs awsLogs() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        return AWSLogsClientBuilder.standard()
                 .withRegion(Regions.US_EAST_1)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
