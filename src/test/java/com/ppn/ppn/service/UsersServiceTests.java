414cf24333197aafb5b9ad4f6ad3de4b55b0eec9
LeTung <66472197+letung999@users.noreply.github.com>
2023-11-25 17:47:27 +0700

    Coverage unit test for user service (#44)
    * implement authentication and fix error security
    
    * refactor name version of dependencies
    
    * active prod environment
    
    * modify relationship entities to get data
    
    * add message status to response
    
    * turn on jpa auditing
    
    * add correlationId
    
    * handle verify email
    
    * override MethodArgumentNotValidException and handle exception
    
    * create role api
    
    * fix error verify email
    
    * remove redudant class
    
    * create a general for response api and modify response at controller
    
    * handle upload file amazon s3
    
    * dÆhandle dÆÆownload file from s3
    
    * implement redis server
    
    * implement search and advance search api
    
    * handle get ÃÃinformation company api
    
    * handle cache data for get all users
    
    * handle cache data search users api
    
    * add a field response data of CompanyProfile
    
    * add READ.ME for config send log from application to cloudWatch
    
    * set up monitoring log by CloudWatch
    
    * add document to set up cloudWatch
    
    * create update users api and invalidate cache
    
    * add payment class
    
    * modify relationship between CompanyProfile and CompanyProfileMapping
    
    * coverage unit tÃest for repository layer
    
    * add builder anotation of lombok
    
    * unit test for usersService
    
    * fix unit test for createUsers
    
    * coverage unit test for user service
    
    ---------
    
    Co-authored-by: letung999 <letung1112000@gmail.com>

--- src/test/java/com/ppn/ppn/service/UsersServiceTests.java
+++ src/test/java/com/ppn/ppn/service/UsersServiceTests.java
@@ -7,6 +7,9 @@ import com.ppn.ppn.entities.Users;
 import com.ppn.ppn.exception.ResourceDuplicateException;
 import com.ppn.ppn.exception.ResourcesNotFoundException;
 import com.ppn.ppn.mapper.UsersMapper;
+import com.ppn.ppn.payload.SearchUserRequest;
+import com.ppn.ppn.payload.SearchUserResponse;
+import com.ppn.ppn.repository.CacheDataRepository;
 import com.ppn.ppn.repository.RoleRepository;
 import com.ppn.ppn.repository.UsersRepository;
 import org.assertj.core.api.Assertions;
@@ -15,12 +18,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 import static com.ppn.ppn.constant.ApprovalStatus.ACTIVE;
 import static com.ppn.ppn.constant.ApprovalStatus.PENDING;
@@ -34,6 +38,8 @@ public class UsersServiceTests {
     private UsersRepository usersRepository;
     @Mock
     private RoleRepository roleRepository;
+    @Mock
+    private CacheDataRepository cacheDataRepository;
     @InjectMocks
     private UsersServiceImpl usersService;
     @Spy
@@ -57,7 +63,7 @@ public class UsersServiceTests {
 
         usersDto = UsersDto.builder()
                 .userId(1)
-                .email("letung012000@gmail.com")
+                .email("c@gmail.com")
                 .firstName("tung")
                 .password(passwordEncoder.encode("123456"))
                 .phoneNumber("0338257409")
@@ -76,7 +82,7 @@ public class UsersServiceTests {
     public void givenUsersObject_whenCreateUsers_thenUsersObject() {
 
         BDDMockito.given(usersRepository.findByEmail(usersDto.getEmail()))
-                .willReturn(Optional.of(users));
+                .willReturn(Optional.empty());
         //action
         BDDMockito.given(roleRepository.findByRoleName(VIEWER))
                 .willReturn(Optional.of(role));
@@ -149,6 +155,149 @@ public class UsersServiceTests {
         BDDMockito.verify(usersRepository, BDDMockito.never()).save(any(Users.class));
     }
 
+    @Test
+    public void givenUsersList_whenGetAll_thenUsersList() {
+        //setup
+        Page<Users> usersPage = BDDMockito.mock(Page.class);
+
+        Mockito.doReturn(getMockListUsers())
+                .when(usersPage).getContent();
+
+        BDDMockito.given(usersRepository.findAll(any(Pageable.class)))
+                .willReturn(usersPage);
+
+        //action
+        List<UsersDto> usersDtoList = usersService.all(PageRequest.of(0, 1));
+
+        //output
+        Assertions.assertThat(usersDtoList).isNotNull();
+    }
+
+    @Test
+    public void givenAllListUsers_whenSearchWithoutFilter_thenReturnAllListUsers() {
+        //setup
+        SearchUserRequest searchRequest = new SearchUserRequest();
+        searchRequest.setPageIndex(1);
+        searchRequest.setPageSize(10);
+
+        //action
+        Pageable pageable = Mockito.mock(Pageable.class);
+        Page<Users> usersPage = Mockito.mock(Page.class);
+
+        //action
+        Mockito.doReturn(usersPage)
+                .when(usersRepository).findAll(any(Specification.class), any(Pageable.class));
+
+        Mockito.doReturn(getMockListUsers()).when(usersPage).getContent();
+
+        SearchUserResponse searchUserResponse = usersService.search(searchRequest, pageable);
+        //output
+        Assertions.assertThat(searchUserResponse).isNotNull();
+        Assertions.assertThat(searchUserResponse.getUserDtoList().size()).isEqualTo(3);
+    }
+
+    @Test
+    public void givenSearchUserResponse_whenSearch_thenSearchUserResponse() {
+        //setup
+        SearchUserRequest searchRequest = new SearchUserRequest();
+        searchRequest.setPageIndex(1);
+        searchRequest.setPageSize(10);
+        searchRequest.setSortBy("firstName");
+        searchRequest.setAscending(true);
+        searchRequest.setEmail("test@example.com");
+        searchRequest.setFirstName("John");
+        searchRequest.setPhoneNumber("+1234567890");
+        searchRequest.setStatus("Active");
+        searchRequest.setGender("Male");
+
+        Pageable pageable = Mockito.mock(Pageable.class);
+        Page<Users> usersPage = Mockito.mock(Page.class);
+
+        //action
+        Mockito.doReturn(usersPage)
+                .when(usersRepository).findAll(any(Specification.class), any(Pageable.class));
+
+        Mockito.doReturn(getMockListUsers()).when(usersPage).getContent();
+
+        SearchUserResponse searchUserResponse = usersService.search(searchRequest, pageable);
+        //output
+        Assertions.assertThat(searchUserResponse).isNotNull();
+        Assertions.assertThat(searchUserResponse.getUserDtoList().size()).isEqualTo(3);
+    }
+
+    @Test
+    public void givenEmptyListUsers_whenSearch_thenEmptyListUsers() {
+        //setup
+        SearchUserRequest searchRequest = new SearchUserRequest();
+        searchRequest.setPageIndex(1);
+        searchRequest.setPageSize(10);
+        searchRequest.setSortBy("firstName");
+        searchRequest.setAscending(true);
+        searchRequest.setEmail("test@example.com");
+        searchRequest.setFirstName("John");
+        searchRequest.setPhoneNumber("+1234567890");
+        searchRequest.setStatus("Active");
+        searchRequest.setGender("Male");
+
+        Pageable pageable = Mockito.mock(Pageable.class);
+        Page<Users> usersPage = Mockito.mock(Page.class);
+
+        //action
+        Mockito.doReturn(usersPage).when(usersRepository)
+                .findAll(any(Specification.class), any(Pageable.class));
+
+        Mockito.doReturn(Collections.emptyList()).when(usersPage).getContent();
+
+        SearchUserResponse searchUserResponse = usersService.search(searchRequest, pageable);
+
+        //output
+        Assertions.assertThat(searchUserResponse.getUserDtoList().size()).isEqualTo(0);
+        Assertions.assertThat(searchUserResponse.getNumOfItems()).isEqualTo(0);
+        Assertions.assertThat(searchUserResponse.getNumOfPage()).isEqualTo(0);
+    }
+
+    @Test
+    public void givenNullObject_whenUpdateUsers_thenThrowResourceNotFoundException() {
+        //setup
+        Mockito.when(usersRepository.findById(usersDto.getUserId())).thenReturn(Optional.empty());
+
+        //output
+        org.junit.jupiter.api.Assertions.assertThrows(ResourcesNotFoundException.class, () -> {
+            usersService.updateUsers(usersDto);
+        });
+
+        Mockito.verify(usersRepository, Mockito.never()).findAll();
+        Mockito.verify(usersRepository, Mockito.never()).save(users);
+        Mockito.verify(cacheDataRepository, Mockito.never()).findAll();
+        Mockito.verify(cacheDataRepository, Mockito.never()).deleteAllById(any());
+    }
+
+    @Test
+    public void given_whenUpdateUsers_thenResourceDuplicateException() {
+        //setup
+        Users userExisted = Users.builder()
+                .email("b@gmail.com")
+                .firstName("tung")
+                .password(passwordEncoder.encode("123456"))
+                .phoneNumber("0338257409")
+                .status(String.valueOf(PENDING))
+                .roles(roles)
+                .build();
+
+        Mockito.when(usersRepository.findById(usersDto.getUserId()))
+                .thenReturn(Optional.of(userExisted));
+        Mockito.doReturn(getMockListUsers()).when(usersRepository).findAll();
+
+        //action and output
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceDuplicateException.class, () -> {
+            usersService.updateUsers(usersDto);
+        });
+        Mockito.verify(usersRepository, Mockito.never()).save(users);
+        Mockito.verify(cacheDataRepository, Mockito.never()).findAll();
+        Mockito.verify(cacheDataRepository, Mockito.never()).deleteAllById(any());
+
+    }
+
     //private methods
     private String randomString(int length) {
         String data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -161,5 +310,76 @@ public class UsersServiceTests {
             resultData.append(data.charAt(index));
         }
         return resultData.toString();
+    }
+
+    private List<Users> getMockListUsers() {
+        return List.of(
+                Users.builder()
+                        .userId(1)
+                        .email("a@gmail.com")
+                        .firstName("tung")
+                        .password(passwordEncoder.encode("123456"))
+                        .phoneNumber("0338257409")
+                        .status(String.valueOf(PENDING))
+                        .roles(roles)
+                        .build(),
+                Users.builder()
+                        .userId(2)
+                        .email("b@gmail.com")
+                        .firstName("tung")
+                        .password(passwordEncoder.encode("123456"))
+                        .phoneNumber("0338257409")
+                        .status(String.valueOf(PENDING))
+                        .roles(roles)
+                        .build(),
+                Users.builder()
+                        .userId(3)
+                        .email("c@gmail.com")
+                        .firstName("tung")
+                        .password(passwordEncoder.encode("123456"))
+                        .phoneNumber("0338257409")
+                        .status(String.valueOf(PENDING))
+                        .roles(roles)
+                        .build()
+        );
+    }
+
+    private List<String> getMockListEmailData() {
+        List<String> emails = Arrays.asList(
+                "user1@example.com",
+                "user1@example.com",
+                "user1@example.com",
+                "user1@example.com",
+                "user1@example.com"
+        );
+        return emails;
+    }
+
+    private SearchUserResponse getMockDataSearchUserResponse() {
+        List<UsersDto> usersList = new ArrayList<>();
+        UsersDto user1 = new UsersDto();
+        user1.setFirstName("John");
+        user1.setEmail("test@example.com");
+        user1.setPhoneNumber("+1234567890");
+        user1.setGender("Male");
+        user1.setStatus("PENDING");
+
+        UsersDto user2 = new UsersDto();
+        user2.setFirstName("Jane");
+        user2.setEmail("jane@example.com");
+        user2.setPhoneNumber("+9876543210");
+        user2.setGender("Female");
+        user2.setStatus("PENDING");
+
+        usersList.add(user1);
+        usersList.add(user2);
+
+        SearchUserResponse searchUserResponse = SearchUserResponse.builder()
+                .numOfItems((long) usersList.size())
+                .numOfPage(1)
+                .userDtoList(usersList)
+                .build();
+
+        return searchUserResponse;
     }
 }
